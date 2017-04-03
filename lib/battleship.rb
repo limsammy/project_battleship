54529f9d4eac4da5a7f62def19ba5eed9256d4cc
Sam Lim <limsammy1@gmail.com>
2017-04-02 18:26:20 -0600

    beginning development of main game loop messages

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,5 +1,32 @@
 require 'pry'
 
 class BattleShip
+  def initialize
+    @message = Message.new
+  end
 
+  def get_input
+    gets.chomp.downcase
+  end
+
+  def start_game
+    @message.welcome
+    game_menu_choice
+  end
+
+  def game_menu_choice
+    input = get_input
+    case input
+    when 'p'
+      choose_difficulty
+    when 'i'
+      show_instructions
+    when 'q'
+      abort('Quitting...')
+  end
+
+  def show_instructions
+    @message.instructions
+    start_game
+  end
 end
--- lib/interface.rb
+++ lib/interface.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 class Interface
+  def welcome
+    puts 'Welcome to BATTLESHIP'
+    puts 'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
+    print '>'
+  end
 
+  def instructions
+    puts 'Define instructions here (interface#instructions)'
+  end
 end
