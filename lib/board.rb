f684958a0d3bc80c873769d325d1402b11693e6f
Sam Lim <limsammy1@gmail.com>
2017-04-02 19:06:45 -0600

    create grid with Array.new, added get_ships method to iterate through a ships collection

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+require_relative 'space'
+
+class Board
+  def initialize(size)
+    @size = size
+    @grid = setup_grid
+    @ships = []
+  end
+
+  def setup_grid
+    Array.new(@size) { Array.new(@size) { Space.new } }
+  end
+
+  def get_ships(ships)
+    ships.each do |name, size, coordinates|
+      @ships.push(Ship.new(name, size, coordinates))
+    end
+  end
+end
